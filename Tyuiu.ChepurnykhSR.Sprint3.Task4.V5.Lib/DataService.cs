29270e6dcb664a47d3de760529237ba93b7f673b
Tyuiu.ChepurnykhSR@outlook.com <winxsofa@bk.ru>
2023-10-29 21:43:03 +0500

    Task4

---
+++ Tyuiu.ChepurnykhSR.Sprint3.Task4.V5.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ChepurnykhSR.Sprint3.Task4.V5.Lib
+{
+    public class DataService : ISprint3Task4V5
+    {
+        public double Calculate(int startValue, int stopValue)
+        {
+            double res = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    res = res + ((Math.Cos(x) - Math.Sin(x)) / x);
+                }
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.ChepurnykhSR.Sprint3.Task4.V5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ChepurnykhSR.Sprint3.Task4.V5.Lib;
+
+namespace Tyuiu.ChepurnykhSR.Sprint3.Task4.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+            Console.WriteLine("* функции y=(cos(x)-sin(x))/x                                             *");
+            Console.WriteLine("* При х = 0 прервать цикл. Полученные значения суммировать.               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда = " + ds.Calculate(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
