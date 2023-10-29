925571c94b2763660531a869a27af6ce4fc71299
Tyuiu.ChepurnykhSR@outlook.com <winxsofa@bk.ru>
2023-10-29 22:28:50 +0500

    Task6

---
+++ Tyuiu.ChepurnykhSR.Sprint3.Task6.V19.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ChepurnykhSR.Sprint3.Task6.V19.Lib;
+
+namespace Tyuiu.ChepurnykhSR.Sprint3.Task6.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 10;
+            int stopValue = 15;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 45;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ChepurnykhSR.Sprint3.Task6.V19/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ChepurnykhSR.Sprint3.Task6.V19.Lib;
+
+namespace Tyuiu.ChepurnykhSR.Sprint3.Task6.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая выводит на экран результат                  *");
+            Console.WriteLine("* данного нам задания                                                     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = 10;
+            int stopValue = 15;
+
+            Console.WriteLine("* старт шага: " + startValue);
+            Console.WriteLine("* конец шага: " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.GetSumTheDivisors(startValue, stopValue));
+
+            Console.ReadLine();
+        }
+    }
+}
